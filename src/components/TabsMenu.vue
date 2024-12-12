da76ecacf5c414212c6abd287e03bb415ac0bbbc
Alexander Israel <ale.israel.12@gmail.com>
2024-12-12 10:16:33 -0300

    feat: apply tailwind styles

--- src/components/TabsMenu.vue
+++ src/components/TabsMenu.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
 defineProps<{
   tabs: string[]
+  selectedCity: string
   setSelectedCity: (name) => void
 }>()
 </script>
 
 <template>
-  <div class="tabs">
+  <div class="border-b border-gray-200 flex flex-row space-x-8 mb-8">
     <div v-for="tab in tabs" :key="tab">
-      <button class="tab" @click="setSelectedCity(tab)">{{ tab }}</button>
+      <button
+        :class="[
+          selectedCity === tab
+            ? 'border-blue-500 text-blue-600'
+            : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+          'whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium',
+        ]"
+        :aria-current="selectedCity === tab ? 'page' : undefined"
+        @click="setSelectedCity(tab)"
+      >
+        {{ tab }}
+      </button>
     </div>
   </div>
 </template>
-
-<style scoped>
-.tabs {
-  display: flex;
-  flex-direction: row;
-  column-gap: 12px;
-  margin-bottom: 24px;
-}
-</style>
