40150fab01890527f7829b2873c49a2a41b98456
Alexander Israel <ale.israel.12@gmail.com>
2024-12-12 02:19:38 -0300

    feat: get basic city weather

--- src/views/CityWeatherView.vue
+++ src/views/CityWeatherView.vue
@@ -1,36 +1,16 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import weatherService from '../services/weather-service.ts'
-// import { getCityData } from '../utils/get-city-data.ts'
+import { getCityData } from '../utils/get-city-data.ts'
 
-const selectedCity = ref('Rio de Janeiro')
+const selectedCity = ref('Rio de janeiro')
 const loading = ref(false)
 const error = ref(null)
 const weather = ref(null)
-// const cityData = await getCityData(cityName)
-// if (cityData) {
-//   const { lat, lon } = cityData
-//   const weather = await getCityForecast(lat, lon)
-//   console.log({ weather })
-// }
-
-// -22.90278,-43.2075
-// export default {
-//   data() {
-//     return {
-//       cityName,
-//       weather: null,
-//     }
-//   },
-//   mounted() {
-//     weatherService.getCityWeather(-22.90278, -43.2075).then((data) => {
-//       console.log(data)
-//       this.weather = data
-//     })
-//   },
-// }
+const weatherIconUrl = ref(null)
 
 watch(selectedCity, fetchData(selectedCity))
+watch(selectedCity)
 
 async function fetchData(cityName) {
   console.log(cityName)
@@ -38,9 +18,14 @@ async function fetchData(cityName) {
   loading.value = true
 
   try {
-    const data = await weatherService.getCityWeather(-22.90278, -43.2075)
-    console.log({ data })
-    weather.value = data
+    const cityData = await getCityData('/cities_20000.csv', selectedCity.value || '')
+    if (cityData) {
+      const { lat, lon } = cityData
+      const data = await weatherService.getCityWeather(lat, lon)
+      console.log({ data })
+      weather.value = data
+      weatherIconUrl.value = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    }
   } catch (err) {
     error.value = err.toString()
   } finally {
@@ -54,17 +39,35 @@ async function fetchData(cityName) {
     <h1>{{ selectedCity }}</h1>
     <div v-if="loading" class="loading">Loading...</div>
     <div v-if="error" class="error">{{ error }}</div>
-    <!-- <div>{{ lat }}{{ lon }}</div> -->
-    <div v-if="weather">{{ weather.weather[0].main }}</div>
+    <div class="current-weather" v-if="weather">
+      <h2>
+        {{ weather.weather[0].main }}
+      </h2>
+      <img
+        class="icon"
+        v-if="weatherIconUrl"
+        :src="weatherIconUrl"
+        alt="{{weather.weather[0].main}}"
+      />
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .page {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.page {
+  min-height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+}
+
+.current-weather {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  height: 48px;
 }
 </style>
